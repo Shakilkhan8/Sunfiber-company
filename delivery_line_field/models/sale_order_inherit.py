e296839ad2ba2fe840140a6e178a732f910fede3
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-11-25 22:27:48 +0500

    Add files via upload

--- delivery_line_field/models/sale_order_inherit.py
+++ delivery_line_field/models/sale_order_inherit.py
@@ -1,20 +1,29 @@
-from datetime import timedelta
+import datetime
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderModel(models.Model):
     _inherit = 'sale.order'
 
     def action_confirm(self):
+        self.confirmation_date = datetime.datetime.now().date()
         res = super(SaleOrderModel, self).action_confirm()
         for rec in self:
             for line in rec.order_line:
-
                 line.move_ids.length = line.product_id.carpet_length
                 line.move_ids.quality_id = line.quality_id.id
                 line.move_ids.design_id = line.design_id.id
                 line.move_ids.width = line.product_id.carpet_width
                 line.move_ids.color = line.product_id.carpet_color
+                line.move_ids.bales = line.bales if not self.env.user.check_company else 0.0
+
+                # updating product bales during confirmation of sale order
+                if not self.env.user.check_company:
+                    if line.product_id.bales < line.bales:
+                        raise ValidationError('Bales on hand of %s' % line.product_id.name + ' are less than available bales !')
+                    else:
+                        line.product_id.bales = line.product_id.bales - line.bales
 
         return res
