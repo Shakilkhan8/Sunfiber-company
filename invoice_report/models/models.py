050d87a98f8140396e62ae223424c2281e8a7072
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-01-18 14:27:58 +0500

    Add files via upload

--- invoice_report/models/models.py
+++ invoice_report/models/models.py
@@ -10,19 +10,22 @@ class InvoiceReportInheritModel(models.Model):
         rec_lst = []
         for line in inv.invoice_line_ids:
             if rec_lst:
-                with_exist = list(filter(lambda item: item if item['design'] ==  line.product_id.categ_id.name and item['quality'] == line.product_id.carpet_quality_id.name and item['grade'] == line.product_id.carpet_grade_id.id  else None, rec_lst ))
+                with_exist = list(filter(lambda item: item if item['design_id'] ==  line.product_id.categ_id.id and item['quality'] == line.product_id.carpet_quality_id.name and item['grade'] == line.product_id.carpet_grade_id.id  else None, rec_lst ))
                 if with_exist:
                     with_exist[0]['amount'] += line.price_subtotal
                     with_exist[0]['length'] += line.product_id.carpet_length
                     with_exist[0]['sqf'] += line.quantity
                     with_exist[0]['quantity'] += line.sqf
                 else:
+                    g_name = line.product_id.carpet_grade_id.name if line.product_id.carpet_grade_id else ""
+
                     rec_lst.append({
                         'p_name': line.name,
                         'width': line.product_id.carpet_width,
                         'length': line.product_id.carpet_length,
                         'quality': line.product_id.carpet_quality_id.name,
-                        'design': line.product_id.categ_id.name,
+                        'design': line.product_id.categ_id.name + " ( " +  line.product_id.carpet_grade_id.name + " ) " if line.product_id.carpet_grade_id else "",
+                        'design_id': line.product_id.categ_id.id,
                         'price': line.price_unit,
                         'quantity': line.sqf,
                         'sqf': line.quantity,
@@ -31,12 +34,14 @@ class InvoiceReportInheritModel(models.Model):
                     })
 
             else:
+               g_name = line.product_id.carpet_grade_id.name if line.product_id.carpet_grade_id else ""
                rec_lst.append({
                    'p_name': line.name,
                    'width': line.product_id.carpet_width,
                    'length': line.product_id.carpet_length,
                    'quality': line.product_id.carpet_quality_id.name,
-                   'design': line.product_id.categ_id.name,
+                   'design': line.product_id.categ_id.name +  line.product_id.categ_id.name + " ( " +  line.product_id.carpet_grade_id.name + " ) " if line.product_id.carpet_grade_id else "",
+                   'design_id': line.product_id.categ_id.id,
                    'price': line.price_unit,
                    'quantity': line.sqf,
                    'sqf': line.quantity,
