671d4d1e7d337dc377406d15582f247d63104a97
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-01-21 15:00:48 +0500

    Add files via upload

--- carpet_product_field/models/carpet_product_fields.py
+++ carpet_product_field/models/carpet_product_fields.py
@@ -1,11 +1,12 @@
-from datetime import datetime,timedelta
+import re
+from datetime import datetime, timedelta
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.osv.expression import expression
 
 
 class CarpetProductFields(models.Model):
     _inherit = 'product.template'
-
 
     carpet_color = fields.Char("Color")
     unit_of_measure = fields.Char("Unit of Measure", default='m', readonly=True)
@@ -20,17 +21,54 @@ class CarpetProductFields(models.Model):
     digital_print_child = fields.Many2one('digital.print.child')
     batch_number = fields.Text('Batch Number')
 
-    # def show_product(self):
-    #     product = self.search([])
-    #     p = product.filtered(lambda item: item.create_date.date() ==  datetime.now().date().today() - timedelta(days=1))
-    #     return {
-    #         'name': 'Product Template',
-    #         'view_mode': 'tree',
-    #         'view_id': False,
-    #         'res_model': 'product.template',
-    #         'domain': [('id', 'in', p.ids)],
-    #         'type': 'ir.actions.act_window',
-    #     }
+    @api.model
+    def create(self, vals):
+        res = super(CarpetProductFields, self).create(vals)
+        if res['batch_number']:
+            product_product = self.env['product.product'].search([('product_tmpl_id', '=', res.id)])
+            if product_product:
+                for rec in product_product:
+                    rec.batch_number = res['batch_number']
+        return res
+
+    # def write(self, vals):
+    #     res = super(InheritProductTemplate, self).write(vals)
+    #     if vals.get('customer_ids'):
+    #         product_product = self.env['product.product'].search([('product_tmpl_id', '=', self.id)])
+    #         if product_product:
+    #             for rec in product_product:
+    #                 rec.customer_ids = vals.get('customer_ids')
+    #     return res
+
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = [] if args is None else args.copy()
+        if not (name == '' and operator == 'ilike'):
+            args += ['|', '|',
+                     ('name', operator, name),
+                     ('batch_number', operator, name),
+                     ('barcode', operator, name)
+                     ]
+        return super(CarpetProductFields, self)._name_search(name=name, args=args, operator=operator,
+                                                               limit=limit, name_get_uid=name_get_uid)
+
+
+class ProductProductInherit(models.Model):
+    _inherit = 'product.product'
+
+    batch_number = fields.Char('Batch Number')
+
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if not (name == '' and operator == 'ilike'):
+            args += ['|', '|',
+                     ('name', operator, name),
+                     ('batch_number', operator, name),
+                     ('barcode', operator, name)
+                     ]
+        return super(ProductProductInherit, self)._name_search(name=name, args=args, operator=operator,
+                                                               limit=limit, name_get_uid=name_get_uid)
 
 class DigitalPrintChildCategory(models.Model):
     _name = 'digital.print.child'
