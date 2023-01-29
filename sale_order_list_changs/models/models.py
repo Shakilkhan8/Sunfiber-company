e9807871ac1c84eca628c3c54c45fe2cb98171d6
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-01-29 13:53:22 +0500

    Add files via upload

--- sale_order_list_changs/models/models.py
+++ sale_order_list_changs/models/models.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import fields, models
 
 
-# class sale_order_list_changs(models.Model):
-#     _name = 'sale_order_list_changs.sale_order_list_changs'
-#     _description = 'sale_order_list_changs.sale_order_list_changs'
+class SaleReport(models.Model):
+    _inherit = "sale.report"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    carpet_length = fields.Char("Carpet Length", readonly=True)
+    square_foot = fields.Char("Saquare Foot", readonly=True)
+
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+        fields['carpet_length'] = ", t.carpet_length as carpet_length"
+        fields['square_foot'] = ", l.square_foot as square_foot"
+        # fields['test'] = ", SUM(10)"
+        groupby += ",t.carpet_length"
+        groupby += ",l.square_foot"
+        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
