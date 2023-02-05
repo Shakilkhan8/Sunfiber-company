f8a7bed0e062a846acc9615a538b2ef5ebaf28f4
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-02-06 01:25:31 +0500

    Add files via upload

--- inventory_xlsx_report/wizard/inventory_wizard.py
+++ inventory_xlsx_report/wizard/inventory_wizard.py
@@ -10,7 +10,7 @@ from datetime import datetime, date, timedelta
 class InventoryReportWizard(models.TransientModel):
     _name = 'inventory.report.wizard'
 
-    design_id = fields.Many2many('product.category')
+    design_id = fields.Many2many('product.category' , domain= lambda self: [('company_id', '=', self.env.user.company_id.id)])
 
     def inventory_report_values(self):
         check_lst = []
@@ -23,11 +23,15 @@ class InventoryReportWizard(models.TransientModel):
                     if dict_exist:
                         color_check = list(filter(lambda item: item['color_name'] == line.carpet_color, dict_exist[0]['color']))
                         if color_check:
-                            color_check[0]['color_qty'] += line.qty_available
+                            if line.carpet_grade_id.name == 'A':
+                                color_check[0]['grade_a_color_qty'] += line.qty_available
+                            else:
+                                color_check[0]['grade_b_color_qty'] += line.qty_available
                         else:
                             dict_exist[0]['color'].append({
                                 'color_name': line.carpet_color,
-                                'color_qty': line.qty_available,
+                                'grade_a_color_qty': line.qty_available,
+                                'grade_b_color_qty': line.qty_available,
                             })
                     else:
                         check_lst.append({
@@ -37,14 +41,16 @@ class InventoryReportWizard(models.TransientModel):
                         color_add_lst = list(filter(lambda item: item['cat_name'] == line.categ_id.name, check_lst))
                         color_add_lst[0]['color'].append({
                             'color_name': line.carpet_color,
-                            'color_qty': line.qty_available
+                            'grade_a_color_qty': line.qty_available,
+                            'grade_b_color_qty': line.qty_available,
                         })
-
                 else:
                     color_lst.append({
                         'color_name': line.carpet_color,
-                        'color_qty': line.qty_available,
+                        'grade_a_color_qty': line.qty_available,
+                        'grade_b_color_qty': line.qty_available,
                     })
+
                     check_lst.append({
                         'cat_name': line.categ_id.name,
                         'color': color_lst,
@@ -62,7 +68,8 @@ class xlsReport(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, partners):
 
         sheet = workbook.add_worksheet("Dail Stock Report")
-        formate_two = workbook.add_format({'font_size': 12,'align': 'center'})
+        formate_two = workbook.add_format({'font_size': 12,'align': 'center', 'bold': True})
+        formate_three = workbook.add_format({'font_size': 10,'align': 'center'})
         formates = workbook.add_format({'align': 'center', 'bold': True})
         sheet.set_column(2, 1, 25)
 
@@ -74,19 +81,24 @@ class xlsReport(models.AbstractModel):
         for record in data['record']:
             sheet.write(row, 1, record['cat_name'], formate_two)
             sheet.write(row + 1, 1, 'Rolls A', formate_two)
+            sheet.write(row + 2, 1, 'Rolls B', formate_two)
 
             total_qty = 0
+            total_b_qty = 0
             row_two +=1
-            col = 2
+            col = 3
             for line in record['color']:
-                col +=1
-                total_qty += line['color_qty']
+                col +=2
+                total_qty += line['grade_a_color_qty']
+                total_b_qty += line['grade_b_color_qty']
                 sheet.write(row, 2, 'Total', formate_two)
                 sheet.write(row, col, line['color_name'], formate_two)
-                sheet.write(row +1, col, line['color_qty'], formate_two)
-                sheet.write(row + 1, 2, total_qty, formate_two)
+                sheet.write(row +1, col, line['grade_a_color_qty'], formate_three)
+                sheet.write(row +2, col, line['grade_b_color_qty'], formate_three)
+                sheet.write(row + 1, 2, total_qty, formate_three)
+                sheet.write(row + 2, 2, total_b_qty, formate_three)
 
             res = 'B'+ str(row)+":"+'M'+ str(row)
             sheet.merge_range(res, '')
-            row = row +3
+            row = row +4
             row_three = row_two + 1
